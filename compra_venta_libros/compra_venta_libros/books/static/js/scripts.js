5b74a5faab21c0a5ca93edee2dea72420e866c6f
ZaiKret <119433709+ZaiKret@users.noreply.github.com>
2025-01-06 01:45:15 -0500

    Delete compra_venta_libros directory

--- compra_venta_libros/compra_venta_libros/books/static/js/scripts.js
+++
@@ -1,9 +0,0 @@
-// Función para confirmar antes de eliminar un libro
-document.querySelectorAll('.confirmar-eliminacion').forEach(function(button) {
-    button.addEventListener('click', function(event) {
-        const confirmation = confirm("¿Estás seguro de que deseas eliminar este libro?");
-        if (!confirmation) {
-            event.preventDefault();
-        }
-    });
-});
--- compra_venta_libros/compra_venta_libros/compra_venta_libros/urls.py
+++
@@ -1,9 +0,0 @@
-# compra_venta_libros/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('books.urls')),  # Incluye las rutas de libros aquí
-    path('accounts/', include('django.contrib.auth.urls')),  # Rutas de autenticación
-]
